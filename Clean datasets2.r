66cc6d0d491c4ce4f8d6e5f4defd959c8ae89c58
lopok908 <60017734+lopok908@users.noreply.github.com>
2022-11-12 16:26:48 +0100

    new

--- Clean datasets2.r
+++ Clean datasets2.r
@@ -2,11 +2,11 @@ library(readxl)
 library("writexl")
 
 
-Buyout_Agreements <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/Buyout_Agreements.csv", header=TRUE)
-rent <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/rent.csv", header=TRUE)
-Eviction_Notices <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/Eviction_Notices.csv", header=TRUE)
+Buyout_Agreements <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/Buyout_Agreements.csv", header=TRUE)
+rent <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/rent.csv", header=TRUE)
+Eviction_Notices <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/Eviction_Notices.csv", header=TRUE)
 October2015Permits <- read_excel("October2015Permits.xlsx") # per provare a pulirne uno
-Parcels <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/Parcels.csv", header=TRUE)
+Parcels <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/Parcels.csv", header=TRUE)
 
 
 
@@ -72,7 +72,7 @@ for( i in 1:length(Eviction_Notices[,1])){
 Eviction_Notices <- Eviction_Notices[,-c(10:14)]
 colnames(Eviction_Notices) <- c('address', 'nhood', 'lat', 'long','month','year','block','street_name', 'street_type')
 
-write.csv(Eviction_Notices,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/Eviction_Notices_Clean.csv")
+write.csv(Eviction_Notices,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/Eviction_Notices_Clean.csv")
 
 
 
@@ -108,7 +108,7 @@ for (i in 1:length(Buyout_Agreements[,1])) {    #preparo le date
 }
 Buyout_Agreements <- Buyout_Agreements[,-c(1,5)]
 colnames(Buyout_Agreements) <- c('buyout_amount', 'address','nhood', 'long','lat', 'month','year')
-write.csv(Buyout_Agreements,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/Buyout_Agreements_Clean.csv")
+write.csv(Buyout_Agreements,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/Buyout_Agreements_Clean.csv")
 
 
 
@@ -122,8 +122,8 @@ rent <- rent[(rent[,2] == 'san francisco'),] #solo s francisco città
 rent <- rent[!(is.na(rent[,7])), ] #rimuovo quelli che non hanno i metri quadri, secondo me sono inutili
 rent <- rent[!(is.na(rent[,4])), ] #rimuovo quelli che non hanno il prezzo, secondo me sono inutili
 rent <- rent[!duplicated(rent),] #elimino rent duplici (fatti nello stesso giorno, alla stessa cifra, nello stesso indirizzo)
-write.csv(rent,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/rent_clean.csv")
-rent_clean <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/rent_clean.csv", header=TRUE)
+write.csv(rent,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/rent_clean.csv")
+rent_clean <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/rent_clean.csv", header=TRUE)
 for (i in 1:length(rent[,1])) {
   temp <- strsplit(rent_clean[i,13], "-")
   rent[i,13] <- temp[[1]][1]
@@ -156,6 +156,83 @@ rent[,15]= rent[,4]/rent[,7]
 rent <- rent[,-c(2:5,6:9,12)]
 colnames(rent) <- c('nhood', 'lat', 'lon', 'year', 'month', 'rent/mq')
 write.csv(rent,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/rent_clean.csv")
+rent_clean <- read.csv("C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/rent_clean.csv", header=TRUE)
+
+
+#Calcolo i df con avg rent per nhood per anno e per mese
+vect_year = paste(rent_clean$year)
+vect_month = paste(rent_clean$month)
+vect_nhood = paste(rent_clean$nhood)
+vect_aus = paste(vect_month,vect_year,vect_nhood)
+vect_aus2 = paste(vect_year,vect_nhood)
+rent_clean$year_nhood = vect_aus2
+rent_clean$month_year_nhood = vect_aus
+rm(vect_aus,vect_aus2,vect_year,vect_nhood,vect_month)
+
+length(unique(rent_clean$year_nhood))
+rent_nhood_yearly = aggregate(rent_clean$`rent.mq`, by = list(rent_clean$year_nhood), FUN = mean)
+names(rent_nhood_yearly)[names(rent_nhood_yearly) == 'Group.1'] <- 'year_nhood'
+names(rent_nhood_yearly)[names(rent_nhood_yearly) == 'x'] <- 'avg_rent.mq'
+
+length(unique(rent_clean$month_year_nhood))
+rent_nhood_monthly = aggregate(rent_clean$`rent.mq`, by = list(rent_clean$month_year_nhood), FUN = mean)
+names(rent_nhood_monthly)[names(rent_nhood_monthly) == 'Group.1'] <- 'nhood_month_year'
+names(rent_nhood_monthly)[names(rent_nhood_monthly) == 'x'] <- 'avg_rent.mq'
+#Adesso son da splittare di nuovo mese-anno e nhood e si può plottare tutto!
+
+for (i in 1:length(rent_nhood_monthly[,1])) {   #preparo bene le coordinate
+  temp <- strsplit(rent_nhood_monthly[i,1], " ")
+  for (j in 1:length(temp[[1]])){
+  rent_nhood_monthly[i,2+j] <- temp[[1]][j]
+  }
+}
+
+
+for( i in 1:length(rent_nhood_monthly[,1])){  #preparo l'address
+  j = 6
+  while (j < 13) {
+    if(!(is.na(rent_nhood_monthly[i,j]))) {
+      rent_nhood_monthly[i,5] = paste(rent_nhood_monthly[i,5], rent_nhood_monthly[i,j], sep=" ")
+      j = j+1
+    }
+      else
+        j = 14
+    
+  }
+}
+rent_nhood_monthly <- rent_nhood_monthly[, -c(6:12)]
+colnames(rent_nhood_monthly) <- c('nhood_month_year', 'avg_rent.mq', 'month','year', 'nhood')
+write.csv(rent_nhood_monthly,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/rent_nhood_monthly.csv")
+
+
+
+for (i in 1:length(rent_nhood_yearly[,1])) {   #preparo bene le coordinate
+  temp <- strsplit(rent_nhood_yearly[i,1], " ")
+  for (j in 1:length(temp[[1]])){
+    rent_nhood_yearly[i,2+j] <- temp[[1]][j]
+  }
+}
+
+
+for( i in 1:length(rent_nhood_yearly[,1])){  #preparo l'address
+  j = 5
+  while (j < 12) {
+    if(!(is.na(rent_nhood_yearly[i,j]))) {
+      rent_nhood_yearly[i,4] = paste(rent_nhood_yearly[i,4], rent_nhood_yearly[i,j], sep=" ")
+      j = j+1
+    }
+    else
+      j = 13
+    
+  }
+}
+rent_nhood_yearly <- rent_nhood_yearly[, -c(5:11)]
+colnames(rent_nhood_yearly) <- c('nhood_month_year', 'avg_rent.mq','year', 'nhood')
+write.csv(rent_nhood_yearly,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/rent_nhood_yearly.csv")
+
+
+
+
 
 
 
@@ -185,7 +262,7 @@ for (i in 1:106) {    #preparo le date
 }
 October2015Permits <- October2015Permits[,-c(1)]
 colnames(October2015Permits) <- c('block', 'lot', 'street_number', 'street_name','street_suffix','new_units_built', 'year', 'month')
-write.csv(October2015Permits,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/October2015Permits_clean.csv")
+write.csv(October2015Permits,"C:/Users/Pietro/Desktop/Pietro/Politecnico/Magistrale/Nonparametric_Statistics/Progetto/ricerca di progetti/Progetto Case SF/SF-houses/October2015Permits_clean.csv")
 
 
 
